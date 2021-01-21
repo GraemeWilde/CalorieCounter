74024df9e1a8a60ca5741d17acb59f427444bbf6
Graeme Wilde <GraemeWilde@hotmail.com>
2021-01-20 21:50:25 -0500

    Initial commit.

--- app/src/main/java/com/wilde/caloriecounter2/MainActivity.kt
+++ app/src/main/java/com/wilde/caloriecounter2/MainActivity.kt
@@ -14,100 +14,82 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.preference.PreferenceManager
 import com.google.android.material.navigation.NavigationView
 
-private const val ACTIVITY_TAG = "Main Activity"
+private const val LOGGER_TAG = "Main Activity"
+private const val DEFAULT_HOME_SCREEN = R.id.journalFragment
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(ACTIVITY_TAG, "ACTIVITY CREATED")
+        Log.d(LOGGER_TAG, "Activity Created")
 
         setContentView(R.layout.activity_main)
 
         val navController = this.findNavController(R.id.nav_host_fragment)
         val navGraph = navController.navInflater.inflate(R.navigation.main_navigation)
 
-        //
+        // Load user setting for home screen
         val homeScreen = PreferenceManager.getDefaultSharedPreferences(this).getString("homeScreen", null)
-        Log.d(ACTIVITY_TAG, "Preference homeScreen: ${homeScreen ?: "NULL"}")
-        if (homeScreen != null) {
-            val field = R.id::class.java.getField(homeScreen)
-            val homeFragment = field.get(null) as Int
-            navGraph.startDestination = homeFragment
-        } else {
-            navGraph.startDestination = R.id.journalFragment
-        }
+        Log.d(LOGGER_TAG, "Preference homeScreen: ${homeScreen ?: "NULL"}")
+        navGraph.startDestination =
+                if (homeScreen != null) {
+                    // Translate fragment text name into resource id int using reflection, since
+                    // listPreference setting can only be a string array (not int)
+                    val field = R.id::class.java.getField(homeScreen)
+                    val homeFragment = field.get(null) as Int
+                    //navGraph.startDestination =
+                    homeFragment
+                } else {
+                    //navGraph.startDestination =
+                    DEFAULT_HOME_SCREEN
+                }
         navController.graph = navGraph
-
-        /*when (homeScreen) {
-            "journalFragment" -> navGraph.startDestination = R.id.journalFragment
-            "foodFragment" -> navGraph.startDestination = R.id.foodFragment
-            "statisticsFragment" -> navGraph.startDestination = R.id.statisticsFragment
-            else -> navGraph.startDestination = R.id.journalFragment
-        }*/
-        //navGraph.startDestination = homeScreen//R.id.journalFragment
-        //navController.graph = navGraph
 
         // Fix for when app is first launched, title is not changing to nav map item title
         title = navController.currentDestination?.label
 
-        //navController.backStack.cle
-
-        /*val drawerNavigationView = findViewById<NavigationView>(R.id.nav_view)
-
-        val navGraphIds = listOf(R.navigation.journal, R.navigation.food)
-
-        val controller = drawerNavigationView.setupWithNavController(setOf()
-        )*/
-
-
-        //(supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
-
-        //val navController = findNavController(R.id.nav_host_fragment)
+        // Set up side drawer for navigation
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        //val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.journalFragment, R.id.foodFragment, R.id.statisticsFragment), drawerLayout)
 
+        // Set up toolbar as action bar and also with nav controller for hamburger menu
+        // functionality
         val toolbar = findViewById<Toolbar>(R.id.my_toolbar)
         setSupportActionBar(toolbar)
-
         toolbar.setupWithNavController(navController, appBarConfiguration)
-        //supportActionBar?.hide()
 
-
+        // Set up the navView fragment container
         val navView = findViewById<NavigationView>(R.id.nav_view)
-
         navView.setupWithNavController(navController)
+
+        // Set up listener for nav drawer item selection
         navView.setNavigationItemSelectedListener(this)
-
-
-        /*if (savedInstanceState == null) {
-            val fragment = JournalFragment()
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.main_content, fragment)
-                .commit()
-        }*/
     }
 
     override fun onBackPressed() {
+        // If navDrawer is open, back button closes it
         val navDrawer = findViewById<DrawerLayout>(R.id.drawer_layout)
         if (navDrawer.isDrawerOpen(GravityCompat.START)) {
             navDrawer.close()
         } else {
+            // Read user settings for home screen
             val homeScreen = PreferenceManager.getDefaultSharedPreferences(this).getString("homeScreen", null)
+            Log.d(LOGGER_TAG, "Preference homeScreen: ${homeScreen ?: "NULL"}")
 
-            Log.d(ACTIVITY_TAG, "Preference homeScreen: ${homeScreen?: "NULL"}")
-
+            // Translate fragment text name into resource id int using reflection, since
+            // listPreference setting can only be a string array (not int)
             val homeFragment =
                     if (homeScreen != null) {
                         val homeScreenField = R.id::class.java.getField(homeScreen)
                         homeScreenField.get(null) as Int
                     } else {
-                        R.id.journalFragment
+                        DEFAULT_HOME_SCREEN
                     }
 
             val navController = findNavController(R.id.nav_host_fragment)
+
+            // If we are at end of back stack (aka back would close), if we are not on home screen
+            // then go to home screen instead of closing (pressing back once more should now close)
             if (supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.childFragmentManager?.backStackEntryCount == 0 && navController.currentDestination!!.id != homeFragment) {
                 navController.navigate(homeFragment, null, NavOptions.Builder().setPopUpTo(navController.currentDestination!!.id, true).build())
             } else {
@@ -117,21 +99,23 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        Log.d(LOGGER_TAG, "Inflate options menu.")
         menuInflater.inflate(R.menu.main_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d(LOGGER_TAG, "Option menu item selected.")
         return if (item.itemId == R.id.settingsFragment) {
             findNavController(R.id.nav_host_fragment).navigate(item.itemId, null)
-            true//item.onNavDestinationSelected(navController)
+            true
         } else {
             super.onOptionsItemSelected(item)
         }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        Log.d(ACTIVITY_TAG, "NAV DRAWER OPTIONS MENU SELECTED")
+        Log.d(LOGGER_TAG, "Nav drawer menu item selected.")
         findNavController(R.id.nav_host_fragment).navigate(item.itemId, null)
         findViewById<DrawerLayout>(R.id.drawer_layout).close()
         return true
--- app/src/main/java/com/wilde/caloriecounter2/SettingsFragment.kt
+++ app/src/main/java/com/wilde/caloriecounter2/SettingsFragment.kt
@@ -7,79 +7,26 @@ import android.util.Log
 import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceFragmentCompat
 
+private const val LOGGER_TAG = "Settings Fragment"
+
 class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
-
         preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
     }
 
-    override fun onResume() {
-        super.onResume()
-
-
-    }
-
-    @SuppressLint("RestrictedApi")
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
-        Log.d("SHARED LISTENER", "Key: $key - ${sharedPreferences.getString(key, "")}")
-        //Resources.getId
-
         if (key == "homeScreen") {
+            // Translate fragment text name into resource id int using reflection, since
+            // listPreference setting can only be a string array (not int)
             val homeScreenField = R.id::class.java.getField(sharedPreferences.getString("homeScreen", "")!!)
-
             val homeFragment = homeScreenField.get(null) as Int
 
+            // Not sure if this is actually necessary
             findNavController().graph.startDestination = homeFragment
 
-            /*val navController = findNavController()
-
-            navController.navigate(homeFragment)
-            val backstackEntry = navController.backStack.removeLast()
-            navController.backStack.addFirst(backstackEntry)
-
-            val destination = navController.graph.getAction(homeFragment)
-
-            //val ClassNavControllerViewModel = NavControllerViewModel::class.java;
-
-
-            val cls = Class.forName("androidx.navigation.NavControllerViewModel")
-
-            val methodNavBackStackEntry = NavBackStackEntry::class.java.constructors *///.getConstructor(android.content.Context::class.java, NavDestination::class.java, Bundle::class.java, LifecycleOwner::class.java, cls)
-            //val methodNavBackStackEntry = NavBackStackEntry::class.java.constructors
-
-            //navController.backStack.addFirst(NavBackStackEntry(requireContext(), navController.graph.findNode(homeFragment)!!, null, null, null))
-
-            //navController.backStack.
-
-            //val dest = navController.currentDestination!!
-            //navController.graph.remove(dest)
-
-
-            //val navGraph = navController.navInflater.inflate(R.navigation.main_navigation)
-
-            //navGraph.startDestination = homeFragment
-            //navGraph.addDestinations(navController.graph)
-
-            //val navGraph = findNavController()
-
-
-            //navGraph.backStack.addFirst(NavBackStackEntry(context, navGraph.navigatorProvider.getNavigator()))
-            //val backStackField = NavController::class.java.getField("mBackStack")
-            /*backStackField.isAccessible = true
-            @Suppress("UNCHECKED_CAST")
-            val arrayDeque = backStackField.get(findNavController()) as java.util.ArrayDeque<NavBackStackEntry>
-            val graph = arrayDeque.first.destination as NavGraph*/
-
-            //arrayDeque.addFirst(NavBackStackEntry(context, ))
-
-
-
-
-
-            Log.d("SHARED LISTENER", "Value: $homeFragment")
-            //Log.d("SHARED LISTENER", "Journal Frag id: " + R.id.journalFragment)
+            Log.d(LOGGER_TAG, "Preference homeScreen: $homeFragment")
         }
     }
 }
