fcfdf84d4a975e6a90a368c418940139f9e652c7
Graeme Wilde <GraemeWilde@hotmail.com>
2021-11-26 21:52:37 -0500

    Update EnumDropDown to use the newer ExposedDowndownMenu(Box) Compose classes, and add KDoc documentation.

--- app/src/main/java/com/wilde/caloriecounter2/composables/other/EnumDropDown.kt
+++ app/src/main/java/com/wilde/caloriecounter2/composables/other/EnumDropDown.kt
@@ -1,7 +1,6 @@
 package com.wilde.caloriecounter2.composables.other
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.*
@@ -10,9 +9,16 @@ import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowDropUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalDensity
 
+/**
+ * Exposed Dropdown field that displays selectable values from a passed in enum class
+ *
+ * @param clazz the enum to use for the values
+ * @param selectedEnum the selected value to be shown
+ * @param modifier a [Modifier] for this EnumDropDown
+ * @param labal a label for this EnumDropDown
+ * @param onSelectedChange the callback that is triggered when a new enum is selected
+ */
 @Composable
 fun <T : Enum<T>> EnumDropDown(
     clazz: Class<T>,
@@ -23,47 +29,19 @@ fun <T : Enum<T>> EnumDropDown(
 ) {
     var expanded by remember { mutableStateOf(false) }
 
-    val tfColors = (@Composable {
-        val textFieldDefaults = TextFieldDefaults.textFieldColors()
-        TextFieldDefaults.textFieldColors(
-            disabledTextColor = textFieldDefaults.textColor(enabled = true).value,
-            disabledTrailingIconColor = textFieldDefaults.trailingIconColor(
-                enabled = true,
-                isError = false
-            ).value,
-            backgroundColor = textFieldDefaults.backgroundColor(enabled = true).value,
-            disabledLabelColor = MaterialTheme.colors.onSurface.copy(ContentAlpha.medium),
-        )
-    })()
-
-    /*val tfColors = TextFieldDefaults.textFieldColors(
-        disabledTextColor = TextFieldDefaults
-            .outlinedTextFieldColors().textColor(enabled = true).value,
-        disabledTrailingIconColor = TextFieldDefaults
-            .outlinedTextFieldColors().trailingIconColor(
-                enabled = true,
-                isError = false
-            ).value,
-        //backgroundColor = Color.Transparent,
-        backgroundColor = TextFieldDefaults
-            .outlinedTextFieldColors().backgroundColor(enabled = true).value,
-        disabledLabelColor = MaterialTheme.colors.onSurface.copy(ContentAlpha.medium),
-    )*/
-
-    /*val tfColors = TextFieldDefaults.textFieldColors(
-        disabledTextColor =
-    )*/
-
-    //MaterialTheme.colors.onSurface.copy(ContentAlpha.medium)
-    var dropDownWidth by remember { mutableStateOf(0) }
-    //var dropDownSize by remember { mutableStateOf(Size.Zero) }
-    //var dropDownPos by remember { mutableStateOf(Offset.Zero) }
-
-    Box(modifier = modifier) {
+    @OptIn(ExperimentalMaterialApi::class)
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = !expanded
+        },
+        modifier = Modifier.width(IntrinsicSize.Min).then(modifier)
+    ) {
         TextField(
             value = selectedEnum.name,
             label = label,
             onValueChange = { },
+            modifier = Modifier.fillMaxWidth(),
             trailingIcon = {
                 Icon(
                     imageVector =
@@ -74,38 +52,13 @@ fun <T : Enum<T>> EnumDropDown(
                     contentDescription = null
                 )
             },
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    expanded = true
-                }
-                .onSizeChanged {
-                    dropDownWidth = it.width
-                },
-            /*.onGloballyPositioned {
-                dropDownPos = it.positionInWindow()
-            }*/
-            enabled = false,
-            colors = tfColors,
+            readOnly = true,
+            colors = ExposedDropdownMenuDefaults.textFieldColors()
         )
-        DropdownMenu(
+        ExposedDropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false },
-            modifier = Modifier.width(
-                with(LocalDensity.current) {
-/*                        Log.d("Width", dropDownWidth.toDp().toString())
-                        Log.d(
-                            "Position",
-                            "${dropDownPos.x.toDp().toString()} - ${
-                                dropDownPos.y.toDp().toString()
-                            }"
-                        )*/
-                    dropDownWidth.toDp()
-                })
         ) {
-            //T::class.java.enumConstants.forEach {  }
-            //val clazz: Class<T> = T::class.java
-
             clazz.enumConstants!!.forEach {
                 DropdownMenuItem(onClick = {
                     onSelectedChange(it)
